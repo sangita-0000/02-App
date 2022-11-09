12574c77cc4209b8a42fe5857144279f1c22ee1c
Sangita Setha <sangitasetha52@gmail.com>
2022-11-09 22:00:05 +0530

    CSL-2:added m1() method

--- Demo.java
+++ Demo.java
@@ -1,10 +1,6 @@
 public class Demo{
-	int a=10;
-	public static void main(String []args){
-		System.out.println("main");
-		m1(10);
-	}
-	static void m1(int a){
+	
+	 void m1(){
 		if(a==5){
 			a++;
 			System.out.println("A :"+a);
